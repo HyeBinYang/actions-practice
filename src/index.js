addec1ce84fe293bdc290c941995ea84b88f92a3
HyeBinYang <skdisk7368@gmail.com>
2022-07-30 17:26:11 +0900

    break tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function factorial(n) {
-  if (n < 0) {
-    throw new Error("음수는 안됩니다!");
-  }
+  //   if (n < 0) {
+  //     throw new Error("음수는 안됩니다!");
+  //   }
 
   if (n == 0) return 1;
   return n * factorial(n - 1);
